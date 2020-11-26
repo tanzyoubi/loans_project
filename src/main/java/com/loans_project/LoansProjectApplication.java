b053a93bd6d88bd897536bf2f39e409632a7f77c
tanzyoubi <2533756659@qq.com>
2020-11-26 20:04:25 +0800

    初次提交

---
+++ src/main/java/com/loans_project/LoansProjectApplication.java
@@ -0,0 +1,13 @@
+package com.loans_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loans_project/LoansProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loans_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
